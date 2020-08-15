debac8472f453a9c778fe60d3f4d48b844918bc1
rhaxoddl3536 <69377259+rhaxoddl3536@users.noreply.github.com>
2020-08-15 13:42:02 +0900

    Add files via upload

---
+++ tastsat.py
@@ -0,0 +1,50 @@
+"""
+while a < 5:
+    print("hi", a)
+    a = a + 1
+"""
+
+"""
+a = 2
+b = 1
+while a < 10:
+    b = 1
+    while b < 10:
+        print("{} x {} = {}".format(a, b, a*b))
+        b = b + 1
+    a = a + 1
+"""
+"""
+a = []
+a.append(3)
+a.append(4)
+print(a[0])
+"""
+
+"""
+a = [2, 3, 4,5]
+
+for i in range(len(a)):
+    print(a[i])
+"""
+"""
+import random
+a = []
+
+for i in range(10):
+    b = random.randrange(1, 100)
+    a.append(b)
+
+print(a)
+c = a[0]
+for j in range(10):
+    if c < a[j]:
+        c = a[j]
+
+print("c", c)
+"""
+a = int(input("숫자"))
+while (a % 2)
+    a == 0 or a == 1
+
+print(a)
