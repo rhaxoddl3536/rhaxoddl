96aba586e3dfecba404f53cac89a098e2c074ed5
rhaxoddl3536 <69377259+rhaxoddl3536@users.noreply.github.com>
2020-09-26 13:40:08 +0900

    Add files via upload

---
+++ ddd.py
@@ -0,0 +1,31 @@
+def add_func(d, e):
+    c = d + e
+    return c
+
+
+def mul_func(a,b):
+    c = a*b
+    return c
+
+a = int(input("Enter the number1: "))
+b = int(input("Enter the number2: "))
+
+f = add_func(a,b)
+print(f)
+
+def func(g, h):
+    i = g - h
+    return i
+
+j = int(input("Enter the number3:"))
+k = int(input("Enter the number4:"))
+
+l = func(j, k)
+print(l)
+
+
+
+
+
+
+
